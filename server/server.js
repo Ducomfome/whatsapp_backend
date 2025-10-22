5cfd114766508f4fca340ae46870f742e274de69
fgghgdghdfhdfg <duardocomfome@gmail.com>
2025-10-21 20:04:32 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('/generate-image-with-city', async (req, res) => {
     const textToPrint = `${city}`;
 
     const finalX = 198;
-    const finalY = 115;
+    const finalY = 125;
 
     image.print(font, finalX, finalY, { text: textToPrint, alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT }, image.bitmap.width, image.bitmap.height);
 
